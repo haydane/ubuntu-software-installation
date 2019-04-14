977a6790e17875c4c7c389c98eb3d0a14ad5d3d0
haydane <haydane9@gmail.com>
2019-04-14 17:46:07 +0700

    configure mysql

--- main.sh
+++ main.sh
@@ -3,6 +3,7 @@ sudo apt install upgrade;
 sudo apt install mysql-server;
 sudo apt install vim;
 sudo apt install mysql-workbench;
-sudo apt install php;
+sudo apt install apache2;
+sudo apt install vlc;
 #sudo apt install evolution;
 
